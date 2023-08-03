bf0c348c1308e3ce24093ed9c04b0f5647f215c8
AyaanM <ayaan.merchant08@gmail.com>
2023-08-03 15:29:56 -0600

    fixed the toggle menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,16 +40,17 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex" 
-          } fixed left-0 top-20 w-fit t p-6 bg-black-gradient right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } fixed right-0 top-20 w-fit t p-6 bg-black-gradient mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+          <ul className=" list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
                 className={`border-b border-gray-600 font-poppins font-medium cursor-pointer text-[16px] ${
                   active === nav.title ? "text-primary" : "text-white"
                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
+                onClick={() => {setActive(nav.title); setToggle(false)}}
+  
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
